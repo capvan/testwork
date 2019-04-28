f6ab75cace8e5c4199a9447483fefd70b51b0aa0
Sergey Krivenko <vanseg@mail.ru>
2019-04-28 23:17:15 +0300

    test

--- main.js
+++ main.js
@@ -15,15 +15,6 @@ loadButton.addEventListener('click', () => {
             console.log(xhr.responseXML);
             // result.innerHTML = xhr.responseXML; 
         }
-
-        const tagCount = document.querySelectorAll('#tagCount');
-        tagCount.addEventListener('click', () => {
-            var linkCount = tagCount.getElementsByTagName('p');
-
-            for (var i = 0; i < elements.lenght; i++) {
-                console.log(i);
-            }
-        })
         // alert(document.getElementsByTagName('p').length);
     }); 
 } );
